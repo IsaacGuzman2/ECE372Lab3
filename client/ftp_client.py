d8d7279cd4c06a9bb429b81c5fb56f6155ce605a
IsaacGuzman2 <guzmange@oregonstate.edu>
2023-05-30 18:22:45 -0700

    Part 1,2 and partially 3 done

--- client/ftp_client.py
+++ client/ftp_client.py
@@ -1,2 +1,90 @@
 import os
 os.chdir('myfiles')
+import socket
+from time import sleep
+from threading import Thread
+import asyncio
+
+IP, DPORT = 'localhost', 8080
+
+# Helper function that converts integer into 8 hexadecimal digits
+# Assumption: integer fits in 8 hexadecimal digits
+def to_hex(number):
+    # Verify our assumption: error is printed and program exists if assumption is violated
+    assert number <= 0xffffffff, "Number too large"
+    return "{:08x}".format(number)
+
+async def recv_intro_message(reader: asyncio.StreamReader):
+    full_data = await reader.readline()
+    return full_data.decode()
+
+    
+
+async def send_long_message(writer: asyncio.StreamWriter, data):
+    await asyncio.sleep(1)
+
+    writer.write(to_hex(len(data)).encode())
+    writer.write(data.encode())
+
+    await writer.drain()
+
+# Function processes messages from client
+async def receive_long_message(reader: asyncio.StreamReader):
+
+    data_length_hex = await reader.readexactly(8)
+
+    data_length = int(data_length_hex, 16)
+    full_data = await reader.readexactly(data_length)
+    return full_data.decode()
+
+
+async def connect(i):
+    reader, writer = await asyncio.open_connection(IP, DPORT)
+
+    """
+    Part 1: Introduction
+    """
+    # TODO: receive the introduction message by implementing `recv_intro_message` above.
+    intro = await receive_long_message(reader)
+    print(intro)
+    x = 1
+    while x < 3:
+
+        password = input("Enter Password: ")
+        await send_long_message(writer, password)
+        message = await receive_long_message(reader)
+        print(message)
+        if message == "NAK incorrectPassword\n":
+
+            #print("Attempt "+i+" of 3")
+            x = x+1
+            
+            print(message)
+
+        if message == "ACK correctPassword\n":
+            print("fuck")
+            break
+    await menu(i,reader,writer)
+
+
+async def menu(i,reader,writer):
+    intro = await recv_intro_message(reader)
+    print(intro)
+    while True:
+        message = input("Enter command >>")
+        await send_long_message(writer,message)
+        answer = await receive_long_message(reader)
+        print(answer)
+    
+
+async def main():
+    tasks = []
+    for i in range(1):
+        tasks.append(connect(str(i).rjust(8, '0')))
+
+    await asyncio.gather(*tasks)
+    print("done")
+
+# Run the `main()` function
+if __name__ == "__main__":
+    asyncio.run(main())
--- server/ftp_server.py
+++ server/ftp_server.py
@@ -7,54 +7,120 @@ os.chdir('myfiles')
 INTERFACE, SPORT = 'localhost', 8080
 CHUNK = 100
 
+# Helper function that converts integer into 8 hexadecimal digits
+# Assumption: integer fits in 8 hexadecimal digits
+def to_hex(number):
+    # Verify our assumption: error is printed and program exists if assumption is violated
+    assert number <= 0xffffffff, "Number too large"
+    return "{:08x}".format(number)
+
+#This function asks for the password
+#Similar setup to send_intro_message
 async def gatekeep(writer):
-    password_message = "Welcome, whats the password?\n"
+    password_message = "Whats the password?\n"
 
-    writer.write(password_message.encode())
-    await writer.drain()
+    await send_long_message(writer, password_message)
 
 
-# TODO: Implement me for Part 1!
+#This function tells the client that they failed the password attempt
+async def denied(writer):
+    password_message = "NAK incorrectPassword\n"
+
+    await send_long_message(writer, password_message)
+
+
+#This function tells the client that they passed the password attempt
+async def granted(writer):
+    password_message = "ACK correctPassword\n"
+
+    await send_long_message(writer, password_message)
+
+
+#Function sends intro message to client
 async def send_intro_message(writer):
+    #Store message into intro_message variable
     intro_message = "Hello! Welcome to my guzmange and flintbr server! I'm majoring in ECE\n"
 
-    writer.write(intro_message.encode())
+    #write encoded intro message to the writer variable
+    writer.write(intro_message.encode()) 
+    
+    #send intro message
     await writer.drain()
 
 
-# TODO: Implement me for Part 2!
+async def send_long_message(writer: asyncio.StreamWriter, data):
+    await asyncio.sleep(1)
+
+    writer.write(to_hex(len(data)).encode())
+    writer.write(data.encode())
+
+    await writer.drain()   
+
+
+# Function processes messages from client
 async def receive_long_message(reader: asyncio.StreamReader):
-    # First we receive the length of the message: this should be 8 total hexadecimal digits!
-    # Note: `socket.MSG_WAITALL` is just to make sure the data is received in this case.
+
     data_length_hex = await reader.readexactly(8)
 
-    # Then we convert it from hex to integer format that we can work with
     data_length = int(data_length_hex, 16)
-
     full_data = await reader.readexactly(data_length)
     return full_data.decode()
 
+async def end_program(writer: asyncio.StreamWriter):
+    print("Shutting down server")
+    writer.close()
+    await writer.wait_closed()
+# TODO - Make function that checks password attempts and either continues the connection or cuts connection
 
+
+#the "middle man" fuction once connection is achieved this function is called to handle the connection
 async def handle_client(reader, writer):
-    """
-    Part 1: Introduction
-    """
-    await gatekeep(writer)
-    # TODO: send the introduction message by implementing `send_intro_message` above.
-    await send_intro_message(writer)
 
+    #ask for password
+    await gatekeep(writer)
+    #recieve password
+    
+    #Check password
+    i = 1
+    while i < 3:
+        # Waits for client to send password and stores it in the "password" variable
+        password = await receive_long_message(reader)
+        print(password)
+        # Check if password is incorrect
+        if password != "1234":
+            i = i+1                                     #Increment the attempt counter
+            await denied(writer)                        # Inform the client that their attempt failed
+        # Check if password is correct
+        if password == "1234":      
+            #print("fuck")               
+            await granted(writer)                    # Inform client that their attempt was correct
+            #print("bitch")
+            await menu(reader,writer)                   # Jump to "Menu" routine to begin command parsing
+    await end_program(writer)
+
+async def menu(reader,writer):
+    #print("asshole")
+    #Send intro message once password is correct
+    await send_intro_message(writer)
+    while True:
+        message = await receive_long_message(reader)
+        print(message)
+        command = message.split()
+        print(message)
+
+        #if command[0] == "list":
+        #if command[0] == "put":
+        #if command[0] == "get":
+        #if command[0] == "remove":
+        if command[0] == "close":
+            insert = "ACK Closing Program"
+            await send_long_message(writer,insert)
+            await end_program(writer)
     """
     Part 2: Long Message Exchange Protocol
     """
-    # TODO: Implement function above
-    message = await receive_long_message(reader)
-
-    # I'm only printing the last 8 characters of the message here because it's long
-    print("done: " +  message[-8:])
-
-    writer.close()
-    await writer.wait_closed()
-
+    
+    
 
 async def main():
     server = await asyncio.start_server(
